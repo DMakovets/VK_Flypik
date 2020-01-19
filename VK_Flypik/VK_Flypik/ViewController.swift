520325829aa1eab637bd731952a14d9895c5319f
DMakovets <57898915+DMakovets@users.noreply.github.com>
2020-01-19 22:16:02 +0300

    Add files via upload

---
+++ VK_Flypik/VK_Flypik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_Flypik
+//
+//  Created by Denis Makovets on 1/19/20.
+//  Copyright © 2020 Denis Makovets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
